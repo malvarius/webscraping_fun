fea2f580a5f16b1ce76829d8f6bc51471214ad25
malvarius <e.luismalvar@gmail.com>
2019-09-27 10:19:10 -0700

    adding mongolab

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 // Using this template, the cheerio documentation,
 // and what you've learned in class so far, scrape a website
 // of your choice, save information from the page in a result array, and log it to the console.
-
+require('dotenv').config()
 var cheerio = require("cheerio");
 var axios = require("axios");
 var logger = require("morgan");
 var mongoose = require('mongoose');
 var ScrapeData = require('./models')
 var express = require('express');
-var PORT = 3000;
+var PORT = process.env.PORT||3000;
 var app = express();
 app.use(logger("dev"));
 // Parse request body as JSON
@@ -26,7 +26,7 @@ app.use(express.static("public"));
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/cruises", { useNewUrlParser: true});
+mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost/cruises", { useNewUrlParser: true});
 //  get to query the DB for cruise info
 app.get('/', function (req, res) {
   ScrapeData.cruises.find({})
